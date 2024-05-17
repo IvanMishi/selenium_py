64730ea56b36740df1d374a3bd2d7e7ad2df2a1c
Ivan Mishin <i.a.m@inbox.ru>
2024-05-17 09:03:41 +0300

    upp file find_elements

--- find_element_test.py
+++ find_element_test.py
@@ -1,3 +1,8 @@
+# незарегестрированный пользователь, переходит по ссылке
+# выбирает элементы
+# заполняет данными
+# успешно регестрируется
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time 
@@ -8,11 +13,11 @@ try:
     browser = webdriver.Chrome()
     browser.get(link)
 
-    input1 = browser.find_element(By.TAG_NAME, value1)
+    input1 = browser.find_element(By.TAG_NAME, "input")
     input1.send_keys("Ivan")
-    input2 = browser.find_element(By.NAME, value2)
+    input2 = browser.find_element(By.NAME, "last_name")
     input2.send_keys("Petrov")
-    input3 = browser.find_element(By.CLASS_NAME, value3)
+    input3 = browser.find_element(By.CLASS_NAME, "form-control.city")
     input3.send_keys("Smolensk")
     input4 = browser.find_element(By.ID, "country")
     input4.send_keys("Russia")
@@ -20,9 +25,9 @@ try:
     button.click()
 
 finally:
-    # успеваем скопировать код за 30 секунд
-    time.sleep(30)
-    # закрываем браузер после всех манипуляций
+    # успевает скопировать код за 10 секунд
+    time.sleep(10)
+    # закрывает браузер после всех манипуляций
     browser.quit()
 
-# не забываем оставить пустую строку в конце файла
+# оставляет пустую строку в конце файла
