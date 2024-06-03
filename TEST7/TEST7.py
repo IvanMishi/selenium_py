7c39b2c21bfba4ec61fa6c9b87c89615c95c62f7
Ivan Mishin <i.a.m@inbox.ru>
2024-06-03 22:54:23 +0300

    upp file TEST7

--- TEST7/TEST7.py
+++ TEST7/TEST7.py
@@ -1,25 +1,33 @@
-# Незарегестрированный пользователь, переходит по ссылке, ищет картинку(сундук с сокровищами отмечает чек-бокс, выбирает радиокнопку (Robots rule!), кликает submit 
+# импортирует необходимые библиотеки
+# импортирует модуль time для работы с ожиданием
+import time 
+# импортирует модуль webdriver из библиотеки selenium для взаимодействия с веб-браузером
+from selenium import webdriver 
+# импортирует модуль By из библиотеки selenium.webdriver.common для использования способа поиска элементов на странице
+from selenium.webdriver.common.by import By
+# импортирует модуль math, который предоставляет математические функции
+import math 
 
-import math
+# функция с мат выражением  для заполнения поля ввода рассчитывает логарифм натуральный от модуля произведения 12 и синуса от целочисленного значения x. 
 def calc(x):
   return str(math.log(abs(12*math.sin(int(x)))))
 
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-import time 
-
+# ссылка на страницу
 link = "http://suninjuly.github.io/get_attribute.html"
 
 
 # Менеджер контекста with/as в Python используется для выполнения определенных действий до и после выполнения блока кода.
 with webdriver.Chrome() as browser:
-
+# открывает браузер Chrome
     browser = webdriver.Chrome()
+# переходит по ссылке
     browser.get(link)
 
  
     x_element = browser.find_element(By.CSS_SELECTOR, "[id='treasure']")
+# извлекает текстовое содержимое найденного элемента
     x = x_element.get_attribute("valuex")
+
     y = calc(x)
 
 
@@ -35,8 +43,16 @@ with webdriver.Chrome() as browser:
     button1 = browser.find_element(By.CSS_SELECTOR, "[type='submit']")
     button1.click()
 
-    # успеваем скопировать код за 10 секунд
-    time.sleep(10)
-# Браузер закроется автоматически после завершения блока `with`
+  
+    # получает alert на веб-странице
+    alert = browser.switch_to.alert
+    # сохраняет текст предупреждения (alert) в переменной actual_result
+    actual_result = alert.text
+    # ждет 2 секунды
+    time.sleep(2)
+    # принимает и закрывает alert путем нажатия кнопки "OK" (accept)
+    alert.accept()
+    print('Ответ', actual_result)
+    # браузер закроется автоматически после завершения блока `with`
 
 # не забываем оставить пустую строку в конце файла
