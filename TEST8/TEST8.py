a58ecba2187033401db5c4b70133398bb27f4ea8
2.12.30.3 <109632953+IvanMishi@users.noreply.github.com>
2024-06-05 00:01:38 +0300

    Update TEST8.py

--- TEST8/TEST8.py
+++ TEST8/TEST8.py
@@ -1,30 +1,42 @@
+# импортирует необходимые библиотеки
+импортирует модуль Select который позволяет управлять выпадающими списками на веб-страницах,
 from selenium.webdriver.support.ui import Select
-from selenium import webdriver
-from selenium.webdriver.common.by import By
+# импортирует модуль time для работы с ожиданием
 import time 
+# импортирует модуль webdriver из библиотеки selenium для взаимодействия с веб-браузером
+from selenium import webdriver 
+# импортирует модуль By из библиотеки selenium.webdriver.common для использования способа поиска элементов на странице
+from selenium.webdriver.common.by import By
 
+# ссылка на страницу
 link = "https://suninjuly.github.io/selects1.html"
 
-try:
+# Менеджер контекста with/as в Python используется для выполнения определенных действий до и после выполнения блока кода.
+with webdriver.Chrome() as browser:
+# открывает браузер Chrome
     browser = webdriver.Chrome()
+# переходит по ссылке
     browser.get(link)
-
+# ищет элементы с числами и получает из них текст
     a_element = browser.find_element(By.CSS_SELECTOR, "[id='num1']").text
     b_element = browser.find_element(By.CSS_SELECTOR, "[id='num2']").text
+# преобразует значения в числовые и складывает их
     sum = (int(a_element)+int(a_element))
 
-# Ищет выпадающий список <select> с дочерними элементами <option>, у каждого из которых есть атрибут value
+# ищет выпадающий список <select> с дочерними элементами <option>, у каждого из которых есть атрибут value
     select1 = Select(browser.find_element(By.TAG_NAME, "select"))
-# Выбирает опции в выпадающем списке по значению
+# выбирает опции в выпадающем списке по значению равной сумме чисел
     select1.select_by_value(str(sum))
     
-   
+# нажимает кнопку 'submit'   
     button = browser.find_element(By.CSS_SELECTOR, "[type='submit']").click()
 
-finally:
+
     # успеваем скопировать код за 10 секунд
     time.sleep(1)
     # закрываем браузер после всех манипуляций
     browser.quit()
+
+    # браузер закроется автоматически после завершения блока `with`
 
 # не забывает оставить пустую строку в конце файла
