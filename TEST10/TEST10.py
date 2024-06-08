92a84346850093d5d1fad4193726377b3e609fbf
2.12.30.3 <109632953+IvanMishi@users.noreply.github.com>
2024-06-08 20:21:50 +0300

    Update TEST10.py

--- TEST10/TEST10.py
+++ TEST10/TEST10.py
@@ -9,12 +9,13 @@ from selenium.webdriver.common.by import By
 # импортирует модуль math, который предоставляет математические функции
 import math 
 
-
+# Функция calc(x) возвращает результат логарифма натурального модуля выражения abs(12*math.sin(int(x)))
 def calc(x):
   return str(math.log(abs(12*math.sin(int(x)))))
 
-
+# ссылка на страницу 
 link = "https://SunInJuly.github.io/execute_script.html"
+
 
 # Менеджер контекста with/as в Python используется для выполнения определенных действий до и после выполнения блока кода.
 with webdriver.Chrome() as browser:
@@ -23,22 +24,21 @@ with webdriver.Chrome() as browser:
 # переходит по ссылке
     browser.get(link)
 
-
-    x_element = browser.find_element(By.CSS_SELECTOR, "[id='input_value']").text
-    
-    input = browser.find_element(By.CSS_SELECTOR, "[id='answer']").send_keys(calc(x_element))
-
- 
+# ищет элемент с математическим выражением и сохраняет текст из этого элемента в переменную x
+    x = browser.find_element(By.CSS_SELECTOR, "[id='input_value']").text
+# ищет поле ввода на странице и вводит значение результата вычисления уравнения, используя функцию calc(x) для вычисления значения переменной x.
+    input = browser.find_element(By.CSS_SELECTOR, "[id='answer']").send_keys(calc(x))
+# ищет кнопку 'submit'
     button = browser.find_element(By.CSS_SELECTOR, "[type='submit']")
-
+# прокручивает страницу так, чтобы кнопка была видимой используя execute_script 
     browser.execute_script("return arguments[0].scrollIntoView(true);", button)
-    
-#
-    check = browser.find_element(By.CSS_SELECTOR, "[id='robotCheckbox']").click()
-#    
-    radio = browser.find_element(By.CSS_SELECTOR, "[id='robotsRule']").click()
-    
 
+  
+# отмечает чекбокс
+    check = browser.find_element(By.CSS_SELECTOR, "[id='robotCheckbox']").click()
+# переключает радиокнопку 
+    radio = browser.find_element(By.CSS_SELECTOR, "[id='robotsRule']").click()
+# нажимает кнопку 'submit'  
     button = browser.find_element(By.CSS_SELECTOR, "[type='submit']").click()
 
 
