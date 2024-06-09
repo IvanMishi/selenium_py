46e6ecad3cc9678cff256c2d666e49e514f70c1b
Ivan Mishin <i.a.m@inbox.ru>
2024-06-09 21:02:43 +0300

    upp test2

--- TEST2/TEST2.py
+++ TEST2/TEST2.py
@@ -6,35 +6,45 @@ from selenium import webdriver
 # импортирует модуль By из библиотеки selenium.webdriver.common для использования способа поиска элементов на странице
 from selenium.webdriver.common.by import By
 
+
 # ссылка на страницу
 link = "http://suninjuly.github.io/simple_form_find_task.html"
 
 # блок try используется для выполнения кода, который может вызвать исключение
 try:
-# открывает браузер Chrome
+    # открывает браузер Chrome
     browser = webdriver.Chrome()
-# переходит по ссылке
+    # переходит по ссылке
     browser.get(link)
-# находит и заполняем поле input1
+    # находит и заполняем поле input1
     input1 = browser.find_element(By.TAG_NAME, "input")
     input1.send_keys("Ivan")
-# находит и заполняем поле input2
+    # находит и заполняем поле input2
     input2 = browser.find_element(By.NAME, "last_name")
     input2.send_keys("Petrov")
-# находит и заполняем поле input3
+    # находит и заполняем поле input3
     input3 = browser.find_element(By.CLASS_NAME, "form-control.city")
     input3.send_keys("Smolensk")
-# находит и заполняем поле input4
+    # находит и заполняем поле input4
     input4 = browser.find_element(By.ID, "country")
     input4.send_keys("Russia")
-# находит и нажимает кнопку
+    # находит и нажимает кнопку
     button = browser.find_element(By.CSS_SELECTOR, "button.btn")
     button.click()
 
+# получает alert на веб-странице
+    alert = browser.switch_to.alert
+    # сохраняет текст предупреждения (alert) в переменной actual_result
+    actual_result = alert.text
+    # принимает и закрывает alert путем нажатия кнопки "OK" (accept)
+    alert.accept()
+# выводит значение переменной actual_result в консоль
+    print('Ответ', actual_result)
+
 # код внутри блока finally будет выполнен в любом случае
 finally:
-# ждет 5 секунд
-    time.sleep(5)
+# ждет 3 секунд
+    time.sleep(3)
 # закрывает браузер
     browser.quit()
 
