e3e854aec0694b0c420bccd31381405410cdfd54
2.12.30.3 <109632953+IvanMishi@users.noreply.github.com>
2024-06-15 19:25:10 +0300

    Update TEST6.py

--- TEST6/TEST6.py
+++ TEST6/TEST6.py
@@ -7,52 +7,54 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 # импортирует модуль math, который предоставляет математические функции
 import math 
+# импортирует модуль re, который позволяет работать с регулярными выражениями для поиска, замены и обработки текстовых данных по заданным правилам.
+import re
+
 
 # функция с мат выражением  для заполнения поля ввода рассчитывает логарифм натуральный от модуля произведения 12 и синуса от целочисленного значения x. 
 def calc(x):
   return str(math.log(abs(12*math.sin(int(x)))))
 
+
 # ссылка на страницу
 link = "https://suninjuly.github.io/math.html"
 
 
-# для того чтобы гарантировать закрытие, даже если произошла ошибка в предыдущих строках, использует конструкцию try/finally
-# блок try используется для выполнения кода, который может вызвать исключение
+# если в коде внутри блока try произойдет какая-то ошибка, то код внутри блока finally выполнится в любом случае
 try:
-# открывает браузер Chrome
+    # открывает браузер Chrome
     browser = webdriver.Chrome()
-# переходит по ссылке
+    # переходит по ссылке
     browser.get(link)
 
-# извлекает текстовое содержимое найденного второго элемента с классом nowrap, который является потомком элемента с классом form-group 
+    # извлекает текстовое содержимое найденного второго элемента с классом nowrap, который является потомком элемента с классом form-group 
     x_element = browser.find_element(By.CSS_SELECTOR, "[class='form-group'] .nowrap:nth-child(2)").text
-# полученный текст использует в функции calc, которая преобразует текст в число, вычисляет результат и возвращает его строковое представление. Затем результат сохраняется в переменную y.
+    # полученный текст использует в функции calc, которая преобразует текст в число, вычисляет результат и возвращает его строковое представление. Затем результат сохраняется в переменную y.
     y = calc(x_element)
 
+    # заполняет поле ввода текста на веб-странице данными из переменной y: ищет элемент <input> с классом form-control, который находится внутри элемента <div> с классом form-group, который в свою очередь является потомком элемента <div> с классом container
+    input1 = browser.find_element(By.CSS_SELECTOR, 'div.container div.form-group input.form-control').send_keys(y)
 
-# заполняет поле ввода текста на веб-странице данными из переменной y: ищет элемент <input> с классом form-control, который находится внутри элемента <div> с классом form-group, который в свою очередь является потомком элемента <div> с классом container
-    input1 = browser.find_element(By.CSS_SELECTOR, 'div.container div.form-group input.form-control')
-    input1.send_keys(y)
+    # отмечает чекбокс на веб-странице: ищет элемент <input> с атрибутом type равным checkbox, который является потомком элемента с классом form-check
+    check1 = browser.find_element(By.CSS_SELECTOR, "div.form-check [type='checkbox']").click()
 
-# отмечает чекбокс на веб-странице: ищет элемент <input> с атрибутом type равным checkbox, который является потомком элемента с классом form-check
-    check1 = browser.find_element(By.CSS_SELECTOR, "div.form-check [type='checkbox']")
-    check1.click()
-
-# выбирает радиокнопку на веб-странице: ищет элемент <input>, который имеет атрибут name со значением ruler и атрибут value со значением robots
-    radio1 = browser.find_element(By.CSS_SELECTOR, "[name='ruler'][value='robots']")
-    radio1.click()
+    # выбирает радиокнопку на веб-странице: ищет элемент <input>, который имеет атрибут name со значением ruler и атрибут value со значением robots
+    radio1 = browser.find_element(By.CSS_SELECTOR, "[name='ruler'][value='robots']").click()
     
-# нажимает кнопку отправки формы на веб-странице: ищет элемент <input> с атрибутом type равным submit, который является прямым потомком элемента <form>
-    button1 = browser.find_element(By.CSS_SELECTOR, "form > [type='submit']")
-    button1.click()
+    # нажимает кнопку отправки формы на веб-странице: ищет элемент <input> с атрибутом type равным submit, который является прямым потомком элемента <form>
+    button1 = browser.find_element(By.CSS_SELECTOR, "form > [type='submit']").click()
+
+# получает alert на веб-странице
+    alert = browser.switch_to.alert
+    # выводит числовое значение полученного текста из alert в консоль в качестве ответа
+    print(' '.join([f'Ответ {number}' for number in re.findall(r'\d+\.\d+', alert.text)]))
+    # принимает и закрывает alert путем нажатия кнопки "OK" (accept)
+    alert.accept()
 
 
 # код внутри блока finally будет выполнен в любом случае
 finally:
-    # успевает скопировать код за 10 секунд
-    time.sleep(10)
     # закрывает браузер
     browser.quit()
-    print(y)
 
 # оставляет пустую строку в конце файла
