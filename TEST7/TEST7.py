bb6ab01b8e9eb563d734d41b4b07244cf2cd4cab
Ivan Mishin <i.a.m@inbox.ru>
2024-06-17 14:53:51 +0300

    upp test 7

--- TEST7/TEST7.py
+++ TEST7/TEST7.py
@@ -25,7 +25,7 @@ with webdriver.Chrome() as browser:
 
  
     x_element = browser.find_element(By.CSS_SELECTOR, "[id='treasure']")
-# извлекает текстовое содержимое найденного элемента
+    # извлекает текстовое содержимое найденного элемента
     x = x_element.get_attribute("valuex")
 
     y = calc(x)
@@ -43,16 +43,12 @@ with webdriver.Chrome() as browser:
     button1 = browser.find_element(By.CSS_SELECTOR, "[type='submit']")
     button1.click()
 
-  
     # получает alert на веб-странице
     alert = browser.switch_to.alert
-    # сохраняет текст предупреждения (alert) в переменной actual_result
-    actual_result = alert.text
-    # ждет 2 секунды
-    time.sleep(2)
+    # выводит числовое значение полученного текста из alert в консоль в качестве ответа
+    print(' '.join([f'Ответ {number}' for number in re.findall(r'\d+\.\d+', alert.text)]))
     # принимает и закрывает alert путем нажатия кнопки "OK" (accept)
     alert.accept()
-    print('Ответ', actual_result)
     # браузер закроется автоматически после завершения блока `with`
 
 # не забываем оставить пустую строку в конце файла
