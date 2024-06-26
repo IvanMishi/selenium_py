7b9a596d2da307c942e5ca475a66a040fbf5431a
Ivan Mishin <i.a.m@inbox.ru>
2024-06-26 23:32:58 +0300

    some upp

--- TEST2/TEST2.py
+++ TEST2/TEST2.py
@@ -13,27 +13,22 @@ link = "http://suninjuly.github.io/simple_form_find_task.html"
 # блок try используется для выполнения кода, который может вызвать исключение
 try:
     # открывает браузер Chrome
-    browser = webdriver.Chrome()
+    driver = webdriver.Chrome()
     # переходит по ссылке
-    browser.get(link)
+    driver.get(link)
     # находит и заполняем поле input1
-    input1 = browser.find_element(By.TAG_NAME, "input")
-    input1.send_keys("Ivan")
+    input1 = driver.find_element(By.TAG_NAME, "input").send_keys("Ivan")
     # находит и заполняем поле input2
-    input2 = browser.find_element(By.NAME, "last_name")
-    input2.send_keys("Petrov")
+    input2 = driver.find_element(By.NAME, "last_name").send_keys("Petrov")
     # находит и заполняем поле input3
-    input3 = browser.find_element(By.CLASS_NAME, "form-control.city")
-    input3.send_keys("Smolensk")
+    input3 = driver.find_element(By.CLASS_NAME, "form-control.city").send_keys("Smolensk")
     # находит и заполняем поле input4
-    input4 = browser.find_element(By.ID, "country")
-    input4.send_keys("Russia")
+    input4 = driver.find_element(By.ID, "country").send_keys("Russia")
     # находит и нажимает кнопку
-    button = browser.find_element(By.CSS_SELECTOR, "button.btn")
-    button.click()
+    button = driver.find_element(By.CSS_SELECTOR, "button.btn").click()
 
 # получает alert на веб-странице
-    alert = browser.switch_to.alert
+    alert = driver.switch_to.alert
     # сохраняет текст предупреждения (alert) в переменной actual_result
     actual_result = alert.text
     # принимает и закрывает alert путем нажатия кнопки "OK" (accept)
@@ -44,6 +39,6 @@ try:
 # код внутри блока finally будет выполнен в любом случае
 finally:
     # закрывает браузер
-    browser.quit()
+    driver.quit()
 
 # не забывает оставить пустую строку в конце файла
