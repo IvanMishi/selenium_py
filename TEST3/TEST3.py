cae7d62b6d5a23f2f3ed266e2176162a1759ff83
Ivan Mishin <i.a.m@inbox.ru>
2024-06-27 22:02:49 +0300

    some_upp

--- TEST3/TEST3.py
+++ TEST3/TEST3.py
@@ -6,7 +6,9 @@ from selenium import webdriver
 # импортирует модуль By из библиотеки selenium.webdriver.common для использования способа поиска элементов на странице
 from selenium.webdriver.common.by import By
 # импортирует модуль math, который предоставляет математические функции
-import math 
+import math
+# импортирует модуль re, который позволяет работать с регулярными выражениями для поиска, замены и обработки текстовых данных по заданным правилам. 
+import re
 
 
 # ссылка на страницу содержащую ссылку для перехода в форму регистрции
@@ -19,51 +21,42 @@ m = str(math.ceil(math.pow(math.pi, math.e)*10000))
 # блок try используется для выполнения кода, который может вызвать исключение
 try:
     # открывает браузер Chrome
-    browser = webdriver.Chrome()
+    driver = webdriver.Chrome()
     # переходит по ссылке
-    browser.get(link)
+    driver.get(link)
     # убеждается что открыта искомая страница
     time.sleep(2)
 
 
     # находит элемент на веб-странице, текст ссылки которого содержит подстроку из переменной m, и нажимает на него
-    link = browser.find_element(By.PARTIAL_LINK_TEXT, m).click()
+    link = driver.find_element(By.PARTIAL_LINK_TEXT, m).click()
 
     # находит и заполняет поле input1 по названию тега
-    input1 = browser.find_element(By.TAG_NAME, "input")
-    input1.send_keys("Ivan")
+    input1 = driver.find_element(By.TAG_NAME, "input").send_keys("Ivan")
 
     # находит и заполняет поле input2 по атрибуту name
-    input2 = browser.find_element(By.NAME, "last_name")
-    input2.send_keys("Petrov")
+    input2 = driver.find_element(By.NAME, "last_name").send_keys("Petrov")
 
     # находит и заполняет поле input3 по значению атрибута class
-    input3 = browser.find_element(By.CLASS_NAME, "form-control.city")
-    input3.send_keys("Smolensk")
+    input3 = driver.find_element(By.CLASS_NAME, "form-control.city").send_keys("Smolensk")
 
     # находит и заполняет поле input4 по if элемента 
-    input4 = browser.find_element(By.ID, "country")
-    input4.send_keys("Russia")
+    input4 = driver.find_element(By.ID, "country").send_keys("Russia")
 
     # находит элемент кнопку с помощью правил на основе CSS и нажимает на нее
-    button = browser.find_element(By.CSS_SELECTOR, "button.btn")
-    button.click()
+    button = driver.find_element(By.CSS_SELECTOR, "button.btn").click()
 
     # получает alert на веб-странице
-    alert = browser.switch_to.alert
-    # сохраняет текст предупреждения (alert) в переменной actual_result
-    actual_result = alert.text
-    # ждет 2 секунды
-    time.sleep(2)
+    alert = driver.switch_to.alert
+    # выводит числовое значение полученного текста из alert в консоль в качестве ответа
+    print(' '.join([f'Ответ {number}' for number in re.findall(r'\d+\.\d+', alert.text)]))
     # принимает и закрывает alert путем нажатия кнопки "OK" (accept)
     alert.accept()
-    # выводит значение переменной actual_result в консоль
-    print('Ответ', actual_result)
 
 
 # код внутри блока finally будет выполнен в любом случае
 finally:
     # закрывает браузер
-    browser.quit()
+    driver.quit()
 
 # оставляет пустую строку в конце файла
