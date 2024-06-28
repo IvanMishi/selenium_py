209f54aabfaa05f667744adc4447e5d5f441d12d
Ivan Mishin <i.a.m@inbox.ru>
2024-06-28 22:45:18 +0300

    upp

--- TEST5/TEST5.py
+++ TEST5/TEST5.py
@@ -16,33 +16,36 @@ link = "http://suninjuly.github.io/find_xpath_form"
 # если в коде внутри блока try произойдет какая-то ошибка, то код внутри блока finally выполнится в любом случае
 try:
     # открывает браузер Chrome
-    browser = webdriver.Chrome()
+    driver = webdriver.Chrome()
     # переходит по ссылке
-    browser.get(link)
+    driver.get(link)
 
-    # находит и заполняет поле input1 с помощью правил на основе X-path
-    input1 = browser.find_element(By.XPATH, "//input[@name='first_name']").send_keys("Ivan")
-    # находит и заполняет поле input2 с помощью правил на основе X-path
-    input2 = browser.find_element(By.XPATH, "//input[@name='last_name']").send_keys("Petrov")
-    # находит и заполняет поле input3 с помощью правил на основе X-path
-    input3 = browser.find_element(By.XPATH, "//input[@class='form-control city']").send_keys("Smolensk")
-    # находит и заполняет поле input4 с помощью правил на основе X-path
-    input4 = browser.find_element(By.XPATH, "//input[@id='country']").send_keys("Russia") 
+    # находит и заполняет поле "First name" с помощью правил на основе X-path
+    input_first_name = driver.find_element(By.XPATH, "//input[@name='first_name']").send_keys("Ivan")
+    # находит и заполняет поле "Last name" с помощью правил на основе X-path
+    input_last_name = driver.find_element(By.XPATH, "//input[@name='last_name']").send_keys("Petrov")
+    # находит и заполняет поле "City" с помощью правил на основе X-path
+    input_city = driver.find_element(By.XPATH, "//input[@class='form-control city']").send_keys("Smolensk")
+    # находит и заполняет поле "Country" с помощью правил на основе X-path
+    input_county = driver.find_element(By.XPATH, "//input[@id='country']").send_keys("Russia") 
 
-    # находит кнопку с помощью правил на основе X-path и нажимает на нее
-    button = browser.find_element(By.XPATH, "//button[@type='submit']").click()
+    # находит кнопку "Submit" с помощью правил на основе X-path и нажимает на нее
+    button_submit = driver.find_element(By.XPATH, "//button[@type='submit']").click()
 
-# получает alert на веб-странице
-    alert = browser.switch_to.alert
+    # получает alert на веб-странице
+    alert = driver.switch_to.alert
     # выводит числовое значение полученного текста из alert в консоль в качестве ответа
     print(' '.join([f'Ответ {number}' for number in re.findall(r'\d+\.\d+', alert.text)]))
     # принимает и закрывает alert путем нажатия кнопки "OK" (accept)
     alert.accept()
 
+except ValueError as e:
+    print(f"Произошла ошибка при обработке значений на веб-странице: {e}")
+    
 
 # код внутри блока finally будет выполнен в любом случае
 finally:
     # закрывает браузер
-    browser.quit()
+    driver.quit()
 
 # оставляет пустую строку в конце файла
