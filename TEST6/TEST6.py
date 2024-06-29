16c8ee8944e22a317b77173bdbcb5b95eb03b66c
2.12.30.3 <109632953+IvanMishi@users.noreply.github.com>
2024-06-29 23:24:43 +0300

    Update TEST6.py

--- TEST6/TEST6.py
+++ TEST6/TEST6.py
@@ -23,29 +23,29 @@ link = "https://suninjuly.github.io/math.html"
 # если в коде внутри блока try произойдет какая-то ошибка, то код внутри блока finally выполнится в любом случае
 try:
     # открывает браузер Chrome
-    driiver = webdriver.Chrome()
+    driver = webdriver.Chrome()
     # переходит по ссылке
-    driiver.get(link)
+    driver.get(link)
 
     # извлекает текстовое содержимое найденного второго элемента с классом nowrap, который является потомком элемента с классом form-group 
-    x_element = driiver.find_element(By.CSS_SELECTOR, "[class='form-group'] .nowrap:nth-child(2)").text
+    x_element = driver.find_element(By.CSS_SELECTOR, "[class='form-group'] .nowrap:nth-child(2)").text
     # полученный текст использует в функции calc, которая преобразует текст в число, вычисляет результат и возвращает его строковое представление. Затем результат сохраняется в переменную y.
     y = calc(x_element)
 
     # заполняет поле ввода текста на веб-странице данными из переменной y: ищет элемент <input> с классом form-control, который находится внутри элемента <div> с классом form-group, который в свою очередь является потомком элемента <div> с классом container
-    input_area = driiver.find_element(By.CSS_SELECTOR, 'div.container div.form-group input.form-control').send_keys(y)
+    input_area = driver.find_element(By.CSS_SELECTOR, 'div.container div.form-group input.form-control').send_keys(y)
 
     # отмечает чекбокс на веб-странице: ищет элемент <input> с атрибутом type равным checkbox, который является потомком элемента с классом form-check
-    checkbox = driiver.find_element(By.CSS_SELECTOR, "div.form-check [type='checkbox']").click()
+    checkbox = driver.find_element(By.CSS_SELECTOR, "div.form-check [type='checkbox']").click()
 
     # выбирает радиокнопку на веб-странице: ищет элемент <input>, который имеет атрибут name со значением ruler и атрибут value со значением robots
-    radio_button = driiver.find_element(By.CSS_SELECTOR, "[name='ruler'][value='robots']").click()
+    radio_button = driver.find_element(By.CSS_SELECTOR, "[name='ruler'][value='robots']").click()
     
     # нажимает кнопку отправки формы на веб-странице: ищет элемент <input> с атрибутом type равным submit, который является прямым потомком элемента <form>
-    button_submit = driiver.find_element(By.CSS_SELECTOR, "form > [type='submit']").click()
+    button_submit = driver.find_element(By.CSS_SELECTOR, "form > [type='submit']").click()
 
 # получает alert на веб-странице
-    alert = driiver.switch_to.alert
+    alert = driver.switch_to.alert
     # выводит числовое значение полученного текста из alert в консоль в качестве ответа
     print(' '.join([f'Ответ {number}' for number in re.findall(r'\d+\.\d+', alert.text)]))
     # принимает и закрывает alert путем нажатия кнопки "OK" (accept)
@@ -57,6 +57,6 @@ except ValueError as e:
 # код внутри блока finally будет выполнен в любом случае
 finally:
     # закрывает браузер
-    driiver.quit()
+    driver.quit()
 
 # оставляет пустую строку в конце файла
