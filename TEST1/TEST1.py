d03cd2fbd179219c999db573d3684898bf3bcf19
Ivan Mishin <i.a.m@inbox.ru>
2024-07-03 21:22:31 +0300

    upp

--- TEST1/TEST1.py
+++ TEST1/TEST1.py
@@ -1,53 +1,45 @@
-
-#импортирует модуль time для работы с ожиданием
+# Импортирует модуль time для работы с ожиданием
 import time 
-# импортирует модуль webdriver из библиотеки selenium для взаимодействия с веб-браузером
+# Импортирует модуль webdriver из библиотеки selenium для взаимодействия с веб-браузером
 from selenium import webdriver 
-
-
-
-
 
 # Переменная expected_result содержит ожидаемый текст результата, который участник тестового сценария ожидает увидеть после успешного завершения
 expected_result = "Thank you for submitting the form!"
 
 
 # открывает браузер Chrome
-driver = webdriver.Chrome()
-
-# ждет 2 секунды
+webdriver = webdriver.Chrome()
+# Ждет 2 секунды
 time.sleep(2)
 
-# переходит по ссылке, метод get сообщает браузеру, что нужно открыть сайт по указанной ссылке
-driver.get("https://suninjuly.github.io/text_input_task.html")
-
+# Переходит по ссылке, метод get сообщает браузеру, что нужно открыть сайт по указанной ссылке
+webdriver.get("https://suninjuly.github.io/text_input_task.html")
 # ждет 2 секунды
 time.sleep(2)
 
 # Находит поле для ввода текста на веб-странице
 # Метод find_element находит элемент на сайте по заданным параметрам, "class name" и "textarea" параметры для поиска конкретного элемента на странице.
 # Когда этот метод будет выполнен, будет возвращен объект, который представляет найденный элемент на веб-странице.
-textarea = driver.find_element("class name", "textarea")
-
-# вводит текст в поле textarea
+textarea = webdriver.find_element("class name", "textarea")
+# Вводит текст в поле textarea
 textarea.send_keys("пользовательский текст")
-# ждет 2 секунды
+# Ждет 2 секунды
 time.sleep(2)
 
-# находит кнопку отправки формы на веб-странице, используя уникальный идентификатор "submit_button"
-submit_button = driver.find_element("id", "submit_button") 
-# нажимает на кнопку отправки формы 
+# Находит кнопку отправки формы на веб-странице, используя уникальный идентификатор "submit_button"
+submit_button = webdriver.find_element("id", "submit_button")
+# Нажимает на кнопку отправки формы
 submit_button.click()
 
 
 # Убеждается, что текст всплывающего окна (alert) соответствует ожидаемому результату. Если текст в алерте не совпадает с ожидаемым текстом, будет выведено сообщение об ошибке.
-# получает alert на веб-странице
-alert = driver.switch_to.alert
-# сохраняет текст предупреждения (alert) в переменной actual_result
+# Получает alert на веб-странице
+alert = webdriver.switch_to.alert
+# Сохраняет текст предупреждения (alert) в переменной actual_result
 actual_result = alert.text
-# ждет 2 секунды
-time.sleep(2)
-# принимает и закрывает alert путем нажатия кнопки "OK" (accept)
+# Ждет 1 секунду
+time.sleep(1)
+# Принимает и закрывает alert путем нажатия кнопки "OK" (accept)
 alert.accept()
 
 
@@ -57,4 +49,4 @@ assert actual_result == expected_result, f"Текст алерта не соот
 print(f'Полученный текст алерта "{actual_result}" соответствует ожидаемому результату.')
 
 # закрывает браузер
-driver.quit()
+webdriver.quit()
--- TEST3/TEST3.py
+++ TEST3/TEST3.py
@@ -1,62 +1,59 @@
-# импортирует необходимые библиотеки
-# импортирует модуль time для работы с ожиданием
-import time 
-# импортирует модуль webdriver из библиотеки selenium для взаимодействия с веб-браузером
-from selenium import webdriver 
-# импортирует модуль By из библиотеки selenium.webdriver.common для использования способа поиска элементов на странице
+# Импортирует необходимые библиотеки
+# Импортирует модуль time для работы с ожиданием
+import time
+# Импортирует модуль webdriver из библиотеки selenium для взаимодействия с веб-браузером
+from selenium import webdriver
+# Импортирует модуль By из библиотеки selenium.webdriver.common для использования способа поиска элементов на странице
 from selenium.webdriver.common.by import By
-# импортирует модуль math, который предоставляет математические функции
+# Импортирует модуль math, который предоставляет математические функции
 import math
-# импортирует модуль re, который позволяет работать с регулярными выражениями для поиска, замены и обработки текстовых данных по заданным правилам. 
+# Импортирует модуль re, который позволяет работать с регулярными выражениями для поиска, замены и обработки текстовых данных по заданным правилам.
 import re
 
 
-# ссылка на страницу содержащую ссылку для перехода в форму регистрции
+# Ссылка на страницу содержащую ссылку для перехода в форму регистрции
 link = "http://suninjuly.github.io/find_link_text"
-# переменная с мат выражением  для поиска нужной ссылки
+# Переменная с мат выражением  для поиска нужной ссылки
 m = str(math.ceil(math.pow(math.pi, math.e)*10000))
 
 
-# для того чтобы гарантировать закрытие, даже если произошла ошибка в предыдущих строках,  использует конструкцию try/finally
-# блок try используется для выполнения кода, который может вызвать исключение
+# Блок try используется для выполнения кода, который может вызвать исключение
 try:
-    # открывает браузер Chrome
-    driver = webdriver.Chrome()
-    # переходит по ссылке
-    driver.get(link)
-    # убеждается что открыта искомая страница
-    time.sleep(2)
+    # Открывает браузер Chrome
+    webdriver = webdriver.Chrome()
+    # Переходит по ссылке
+    webdriver.get(link)
+    # Убеждается что открыта искомая страница
+    time.sleep(1)
 
 
-    # находит элемент на веб-странице, текст ссылки которого содержит подстроку из переменной m, и нажимает на него
-    link = driver.find_element(By.PARTIAL_LINK_TEXT, m).click()
+    # Находит элемент на веб-странице, текст ссылки которого содержит подстроку из переменной m, и нажимает на него
+    element_link = webdriver.find_element(By.PARTIAL_LINK_TEXT, m).click()
 
-    # находит и заполняет поле input1 по названию тега
-    input1 = driver.find_element(By.TAG_NAME, "input").send_keys("Ivan")
+    # Находит и заполняет поле "First name" по названию тега
+    input_first_name = webdriver.find_element(By.TAG_NAME, "input").send_keys("Ivan")
+    # Находит и заполняет поле "Last name" по атрибуту name
+    input_last_nam = webdriver.find_element(By.NAME, "last_name").send_keys("Petrov")
+    # Находит и заполняет поле "City" по значению атрибута class
+    input_сity = webdriver.find_element(By.CLASS_NAME, "form-control.city").send_keys("Smolensk")
+    # Находит и заполняет поле "Country" по id элемента
+    input4 = webdriver.find_element(By.ID, "country").send_keys("Russia")
+    # Находит и нажимает кнопку Submit с помощью правил на основе CSS
+    button_submit = webdriver.find_element(By.CSS_SELECTOR, "button.btn").click()
 
-    # находит и заполняет поле input2 по атрибуту name
-    input2 = driver.find_element(By.NAME, "last_name").send_keys("Petrov")
-
-    # находит и заполняет поле input3 по значению атрибута class
-    input3 = driver.find_element(By.CLASS_NAME, "form-control.city").send_keys("Smolensk")
-
-    # находит и заполняет поле input4 по if элемента 
-    input4 = driver.find_element(By.ID, "country").send_keys("Russia")
-
-    # находит элемент кнопку с помощью правил на основе CSS и нажимает на нее
-    button = driver.find_element(By.CSS_SELECTOR, "button.btn").click()
-
-    # получает alert на веб-странице
-    alert = driver.switch_to.alert
-    # выводит числовое значение полученного текста из alert в консоль в качестве ответа
+    # Получает alert на веб-странице
+    alert = webdriver.switch_to.alert
+    # Сохраняет текст предупреждения (alert) в переменной actual_result
+    actual_result = alert.text
+    # Выводит числовое значение полученного текста из alert в консоль в качестве ответа
     print(' '.join([f'Ответ {number}' for number in re.findall(r'\d+\.\d+', alert.text)]))
-    # принимает и закрывает alert путем нажатия кнопки "OK" (accept)
+    # Принимает и закрывает alert путем нажатия кнопки "OK" (accept)
     alert.accept()
 
 
-# код внутри блока finally будет выполнен в любом случае
+# Код внутри блока finally будет выполнен в любом случае
 finally:
-    # закрывает браузер
-    driver.quit()
+    # Закрывает браузер
+    webdriver.quit()
 
-# оставляет пустую строку в конце файла
+# Не забывает оставить пустую строку в конце файла
