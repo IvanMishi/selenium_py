2427b202628605ca12d1985358ba1664b87c86e5
Ivan Mishin <i.a.m@inbox.ru>
2024-07-15 23:01:09 +0300

    upp

--- test_12(switch_to_alert)/test_12_A.py
+++ test_12(switch_to_alert)/test_12_A.py
@@ -1,40 +1,58 @@
-from selenium.webdriver.support.ui import Select
+# Импортирует необходимые библиотеки
+# Импортирует модуль time для работы с ожиданием
+import time
+# Импортирует модуль webdriver из библиотеки selenium для взаимодействия с веб-браузером
 from selenium import webdriver
+# Импортирует модуль By из библиотеки selenium.webdriver.common для использования способа поиска элементов на странице
 from selenium.webdriver.common.by import By
-import time 
-
+# Импортирует модуль math, который предоставляет математические функции
 import math
+# Импортирует модуль re, который позволяет работать с регулярными выражениями для поиска, замены и обработки текстовых данных по заданным правилам.
+import re
+
+
+# Функция calc(x) возвращает результат логарифма натурального модуля выражения abs(12*math.sin(int(x)))
 def calc(x):
-  return str(math.log(abs(12*math.sin(int(x)))))
+    return str(math.log(abs(12 * math.sin(int(x)))))
 
-link = "http://suninjuly.github.io/alert_accept.html"
+# Ссылка на страницу
+link = "https://suninjuly.github.io/alert_redirect.html?"
+# Измеряет время выполнения определенного участка кода.
+start = time.time()
 
-try:
-    browser = webdriver.Chrome()
-    browser.get(link)
 
-    
 
-    btn1 = browser.find_element(By.CSS_SELECTOR, "[type='submit']")
-    btn1.click()
-    confirm = browser.switch_to.alert
-    confirm.accept()
 
-    x_element = browser.find_element(By.CSS_SELECTOR, "[id='input_value']")
+
+# Менеджер контекста with/as в Python используется для выполнения определенных действий до и после выполнения блока кода.
+# Открывает браузер Chrome
+with webdriver.Chrome() as webdriver:
+    # Переходит по ссылке
+    webdriver.get(link)
+    # Убеждается что открыта искомая страница
+    time.sleep(1)
+
+    # Находит элемент с математическим выражением, сохраняет текст из этого элемента в переменную x и вычисляет  функцией calc, результат сохраняет в переменную y.
+    x_element = webdriver.find_element(By.CSS_SELECTOR, "[id='input_value']")
     x = x_element.text
     y = calc(x)
 
-    input1 = browser.find_element(By.CSS_SELECTOR, "[id='answer']")
-    input1.send_keys(y)
-    
-    button1 = browser.find_element(By.CSS_SELECTOR, "[type='submit']")
-    button1.click()
+    # Заполняет полe, нажимает кнопку 'submit'
+    answer_area = webdriver.find_element(By.CSS_SELECTOR, "[id='answer']").send_keys(y)
+    button_submit = webdriver.find_element(By.CSS_SELECTOR, "[type='submit']").click()
 
+    # Получает alert на веб-странице
+    alert = webdriver.switch_to.alert
+    # Сохраняет текст предупреждения (alert) в переменной actual_result
+    actual_result = alert.text
+    # Выводит числовое значение полученного текста из alert в консоль в качестве ответа
+    print(' '.join([f'Ответ {number}' for number in re.findall(r'\d+\.\d+', alert.text)]))
+    # Принимает и закрывает alert путем нажатия кнопки "OK" (accept)
+    alert.accept()
 
-finally:
-    # успеваем скопировать код за 10 секунд
-    time.sleep(10)
-    # закрываем браузер после всех манипуляций
-    browser.quit()
+    # Завершение отсчета времени
+    end = time.time()
+    print(f"Время выполнения: {end - start} секунд.")
 
-# не забываем оставить пустую строку в конце файла
+    # Браузер закроется автоматически после завершения блока `with`
+    # Не забывает оставить пустую строку в конце файла
