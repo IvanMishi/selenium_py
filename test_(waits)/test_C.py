a7d64b23eceb6bc4efca481ffd895b44d2d9e267
Ivan Mishin <i.a.m@inbox.ru>
2024-08-21 12:20:58 +0300

    upp

--- test_(waits)/test_C.py
+++ test_(waits)/test_C.py
@@ -1,34 +1,27 @@
-# Импортирует необходимые библиотеки
-# Импортирует модуль time для работы с ожиданием
-import time
-# Импортирует модуль webdriver из библиотеки selenium для взаимодействия с веб-браузером
-from selenium import webdriver
-# Импортирует модуль By из библиотеки selenium.webdriver.common для использования способа поиска элементов на странице
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+import time  # Модуль для работы с функцией ожидания
+from selenium import webdriver  # Модуль для взаимодействия с веб-браузерами
+from selenium.webdriver.common.by import By  # Модуль для определения способов поиска элементов на странице
+from selenium.webdriver.support.ui import WebDriverWait  # Модуль для реализации явно-ожидаемых условий
+from selenium.webdriver.support import expected_conditions as EC  # Модуль для работы с ожидаемыми условиями
 
 
 # Ссылка на страницу
 link = 'https://parsinger.ru/expectations/6/index.html'
-# Измеряет время выполнения определенного участка кода.
+# Измеряет время выполнения
 start = time.time()
-
 
 with webdriver.Chrome() as webdriver:
     # Переходит по ссылке
     webdriver.get(link)
-    # Убеждается что открыта искомая страница
-    time.sleep(1)
+    time.sleep(1) # Убеждается что открыта искомая страница
 
-    # Ожидает 5 сек пока кнопка на странице не станет активной и нажимает на нее если она кликабельна
+    # Ожидает 5 сек кликабельности кнопки на странице
     button = WebDriverWait(webdriver, 5).until(EC.element_to_be_clickable((By.ID, "btn"))).click()
-    time.sleep(1)
-    # Ожидает точного совпадения заголовка, если заголово совпадает выводит текст из элемента с id='result' в консоль
+    # Ожидает присутствия элемента, если элемент с искомым классом присутствует на странице выводит текст из элемента с id='result' в консоль
     print(f'Ответ:{WebDriverWait(webdriver, 30).until(EC.presence_of_element_located((By.CLASS_NAME, 'BMH21YY'))).text}')
 
     # Завершение отсчета времени
     end = time.time()
     print(f"Время выполнения: {end - start} секунд.")
-    # Браузер закроется автоматически после завершения блока `with`
-    # Не забывает оставить пустую строку в конце файла
+
+    # Браузер закрывается автоматически после завершения блока `with`
