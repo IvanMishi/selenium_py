a5823af5a310a4a059e4def9ad83e7a0ccf92f25
Ivan Mishin <i.a.m@inbox.ru>
2024-08-30 22:41:39 +0300

    upp

--- test_(drag_and_drop)/test_B.py
+++ test_(drag_and_drop)/test_B.py
@@ -1,29 +1,37 @@
-# Импортирует необходимые библиотеки для работы с Selenium и ожиданиями
-import time
-from selenium import webdriver
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.common.by import By
+import time  # Модуль для работы с функцией ожидания
+from selenium import webdriver  # Модуль для взаимодействия с веб-браузерами
+from selenium.webdriver.common.by import By  # Модуль для определения способов поиска элементов на странице
+from selenium.webdriver.common.action_chains import ActionChains # Импортирует класс ActionChains для выполнения сложных пользовательских действий (клик, перемещение мыши, перетаскивание).
 
-# Используемт контекстный менеджер для автоматического закрытия драйвера после выполнения действий
+# Ссылка на страницу
+link = 'https://parsinger.ru/draganddrop/3/index.html'
+# Измеряет время выполнения
+start = time.time()
+
 with webdriver.Chrome() as webdriver:
-    # Открывает веб-страницу по указанному URL
-    webdriver.get("https://parsinger.ru/draganddrop/3/index.html")
-    # Убеждается что открыта искомая страница
-    time.sleep(1)
-    # Находит элемент, для перемещения
-    drag = webdriver.find_element(By.ID, "block1")
-    # Находит элемент, на который перемесщает
-    controlPoints = webdriver.find_elements(By.CLASS_NAME, "controlPoint")
-    # Выполняет операцию захвата и удержания элемента
-    ActionChains(webdriver).click_and_hold(drag).perform()
-    # Выполняет операцию перемещения удерживаемого элемента в цикле по контрольным точкам
-    for point in controlPoints:
-        ActionChains(webdriver).move_to_element(point).perform()
-    #Выполняет оерацию отпускания удерживаемого элемента после перемещения по контролным точкам
-    ActionChains(webdriver).release(drag).perform()
+    # Переходит по ссылке
+    webdriver.get(link)
+    time.sleep(1) # Убеждается что открыта искомая страница
 
+    ## Находит элемент, для перемещения
+    drag_element = webdriver.find_element(By.ID, "block1")
+    # Находит элемент, на который перемесщает
+    drop_element_points = webdriver.find_elements(By.CLASS_NAME, "controlPoint")
+    # Выполняет операцию захвата и удержания элемента
+    ActionChains(webdriver).click_and_hold(drag_element).perform()
+    # Выполняет операцию перемещения удерживаемого элемента в цикле по контрольным точкам
+    for point in drop_element_points:
+        ActionChains(webdriver).move_to_element(point).perform()
+    # Выполняет оерацию отпускания удерживаемого элемента после перемещения по контролным точкам
+    ActionChains(webdriver).release(drag_element).perform()
     # Визуально убеждается, что элемент был перемещен
     time.sleep(1)
-
     # Находит элемент с результатом по его ID и выводим его текст
     print(f'Ответ: {webdriver.find_element(By.ID, 'message').text}')
+
+
+    # Завершение отсчета времени
+    end = time.time()
+    print(f"Время выполнения: {end - start} секунд.")
+
+    # Браузер закрывается автоматически после завершения блока `with`
