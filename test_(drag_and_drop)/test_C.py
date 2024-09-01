47508ec81b4083034f55b3cf5467bef57632a3ca
Ivan Mishin <i.a.m@inbox.ru>
2024-09-01 23:21:45 +0300

    upp

--- test_(drag_and_drop)/test_C.py
+++ test_(drag_and_drop)/test_C.py
@@ -1,35 +1,31 @@
 import time  # Модуль для работы с функцией ожидания
 from selenium import webdriver  # Модуль для взаимодействия с веб-браузерами
 from selenium.webdriver.common.by import By  # Модуль для определения способов поиска элементов на странице
-from selenium.webdriver.common.action_chains import ActionChains # Импортирует класс ActionChains для выполнения сложных пользовательских действий (клик, перемещение мыши, перетаскивание).
-from selenium.webdriver.support.color import Color # Импортирует класс Color для работы с цветами в веб-элементах.
-
+from selenium.webdriver.common.action_chains import \
+    ActionChains  # Импортирует класс ActionChains для выполнения сложных пользовательских действий (клик, перемещение мыши, перетаскивание).
 
 # Ссылка на страницу
-link = 'https://parsinger.ru/selenium/5.10/3/index.html'
+link = 'https://parsinger.ru/selenium/5.10/2/index.html'
 # Измеряет время выполнения
 start = time.time()
 
-with webdriver.Chrome() as webdriver: # Создаёт экземпляр драйвера Chrome и автоматически закрывает его по завершении блока кода.
-    webdriver.get(link) # Переходит по ссылке
-    time.sleep(1) # Убеждается что открыта искомая страница
+with webdriver.Chrome() as webdriver:  # Создаёт экземпляр драйвера Chrome и автоматически закрывает его по завершении блока кода.
+    # Устанавливает размер окна при запуске браузера
+    webdriver.set_window_size(1200, 900)
+    webdriver.get(link)  # Переходит по ссылке
+    time.sleep(1)  # Убеждается что открыта искомая страница
 
-    # Находит элементы для перемещения и броска в область
-    drag_elements = webdriver.find_elements(By.CLASS_NAME, 'draganddrop')
-    drop_elements = webdriver.find_elements(By.CLASS_NAME, 'draganddrop_end')
-
-    # Действия с перетаскиванием элементов
-    for drag, drop in zip(drag_elements, drop_elements):
-        if Color.from_string(drag.value_of_css_property('background-color')).rgb == Color.from_string(
-                drop.value_of_css_property('border-color')).rgb:
-            ActionChains(webdriver).click_and_hold(drag).move_to_element(drop).release().perform()
-            time.sleep(.1)
-    time.sleep(1) # Визуально убеждается, что элемент был перемещен
-
+    # Находит элементы: которые будет перетаскивать и куда будет перетаскивать
+    drag_list = webdriver.find_elements(By.CLASS_NAME, "ui-draggable")
+    drop_area = webdriver.find_element(By.CLASS_NAME, 'draganddrop_end')
+    # Действия с перетаскиванием элементов на расстояние из разницы между шириной экрана и шириной элемента
+    for element in drag_list:
+        ActionChains(webdriver).drag_and_drop_by_offset(element, (
+                webdriver.get_window_size()["width"] - (drop_area.size['width'])), 0).perform()
     # Находит элемент с результатом по его ID и выводит его текст
     print(f'Ответ: {webdriver.find_element(By.ID, 'message').text}')
 
-    # Завершение отсчета времени
-    end = time.time()
-    print(f"Время выполнения: {end - start} секунд.")
-    # Браузер закрывается автоматически после завершения блока `with`
+# Завершение отсчета времени
+end = time.time()
+print(f"Время выполнения: {end - start} секунд.")
+# Браузер закрывается автоматически после завершения блока `with`
