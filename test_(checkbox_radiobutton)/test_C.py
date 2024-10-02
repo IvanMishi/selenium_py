744c910de1098953164529a238d88f31516b22ac
Ivan Mishin <i.a.m@inbox.ru>
2024-10-02 19:32:03 +0300

    upp

---
+++ test_(checkbox_radiobutton)/test_C.py
@@ -0,0 +1,38 @@
+import time  # Модуль для работы с функцией ожидания
+from selenium import webdriver  # Модуль для взаимодействия с веб-браузерами
+from selenium.webdriver.common.by import By  # Модуль для определения способов поиска элементов на странице
+
+# Ссылка на страницу.
+link = 'http://parsinger.ru/selenium/5/5.html'
+# Измеряет время выполнения.
+start = time.time()
+
+# Чтение чисел из текстового файла
+with open('numbers_by_С.txt', 'r') as file:
+    line = file.readline()
+    numbers = list(map(int, line.split(',')))
+
+
+with webdriver.Chrome() as webdriver:  # Создаёт экземпляр драйвера Chrome и автоматически закрывает его по завершении блока кода.
+    webdriver.get(link)  # Переходит по ссылке.
+    time.sleep(1)  # Убеждается что открыта искомая страница.
+
+    # Находит все элементы чекбокс на веб странице
+    checkboxs = webdriver.find_elements(By.CSS_SELECTOR, "[type='checkbox']")
+
+    # Перебирает каждый найденный элемент в списке checkboxs
+    for element in checkboxs:
+        value = int(element.get_attribute('value'))
+        if value in numbers:
+            element.click() # Отмечает каждый найденый чекбокс
+
+    # Нажимает кнопку отправки формы на веб-странице
+    button_submit = webdriver.find_element(By.CSS_SELECTOR, "[type='button']").click()
+    time.sleep(1)  # Визуально убеждается, что все действия выполнены.
+    # Находит элемент с результатом по его ID и выводит его текст
+    print(f'Ответ: {webdriver.find_element(By.ID, 'result').text}')
+
+    # Завершение отсчета времени
+    end = time.time()
+    print(f"Время выполнения: {end - start} секунд.")
+    # Браузер закрывается автоматически после завершения блока `with`
