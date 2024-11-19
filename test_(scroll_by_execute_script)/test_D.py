73829834e9287fe365c15331ab7fc1b1ae0e7ab7
Ivan Mishin <i.a.m@inbox.ru>
2024-11-19 23:33:37 +0300

    upp

--- test_(scroll_by_execute_script)/test_D.py
+++ test_(scroll_by_execute_script)/test_D.py
@@ -1,27 +1,26 @@
-# Импортирует необходимые библиотеки
-import time
-# Импортирует модуль webdriver из библиотеки selenium для взаимодействия с веб-браузером
-from selenium import webdriver
-# Импортирует модуль By из библиотеки selenium.webdriver.common для использования способа поиска элементов на странице
-from selenium.webdriver.common.by import By
+import time  # Модуль для работы с функцией ожидания
+from selenium import webdriver  # Модуль для взаимодействия с веб-браузерами
+from selenium.webdriver.common.by import By  # Модуль для определения способов поиска элементов на странице
 
 
-# Измеряет время выполнения определенного участка кода.
+# Ссылка на страницу.
+link = 'https://parsinger.ru/selenium/5.7/4/index.html'
+# Измеряет время выполнения.
 start = time.time()
-# Открывает браузер Chrome
-with webdriver.Chrome() as webdriver:
-    # Переходит по ссылке
-    webdriver.get("https://parsinger.ru/selenium/5.7/4/index.html")
-    # Убеждается что открыта искомая страница
-    time.sleep(1)
-    # Прокручивает страницу до тех пор, пока на странице не будет как минимум 1000 элементов input
+
+with webdriver.Chrome() as webdriver:  # Создаёт экземпляр драйвера Chrome и автоматически закрывает его по завершении блока кода.
+    webdriver.get(link)  # Переходит по ссылке.
+    time.sleep(1)  # Убеждается что открыта искомая страница.
+
+
+    # Прокручивает страницу до тех пор, пока на ней не появляется как минимум 1000 элементов типа input.
     while len(webdriver.find_elements(By.CSS_SELECTOR, "input")) < 1000:
         # Находит последний дочерний контейнер на странице
         last = webdriver.find_element(By.CSS_SELECTOR, ".child_container:last-child")
         # Прокручивает последний дочерний элемент в видимую область
         webdriver.execute_script("return arguments[0].scrollIntoView(true)", last)
 
-    # Перебираем все элементы чекбоксов на странице
+    # Перебирает все элементы чекбоксов на странице.
     for checkbox in webdriver.find_elements(By.CSS_SELECTOR, "input"):
         # Проверяет, является ли значение чекбокса четным
         if int(checkbox.get_attribute("value")) % 2 == 0:
@@ -34,6 +33,8 @@ with webdriver.Chrome() as webdriver:
     webdriver.find_element(By.CSS_SELECTOR, ".alert_button").click()
     # Выводит текст из диалогового окна alert в качестве ответа
     print(f'Ответ: {webdriver.switch_to.alert.text}')
+
 # Завершение отсчета времени
 end = time.time()
 print(f"Время выполнения: {end - start} секунд.")
+# Браузер закрывается автоматически после завершения блока `with`
