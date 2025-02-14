9fa8203ad1dce03dcafc2bd05cab71c5c7a16bea
Ivan Mishin <i.a.m@inbox.ru>
2025-02-14 22:20:37 +0300

    upp

---
+++ test_(proxy)/test_B.py
@@ -0,0 +1,16 @@
+import time
+from selenium.webdriver.common.by import By
+from seleniumwire import webdriver
+
+options = {'proxy': {
+    'http': "socks5://D2Frs6:75JjrW@194.28.210.39:9867",
+    'https': "socks5://D2Frs6:75JjrW@194.28.210.39:9867",
+    }}
+
+url = 'https://2ip.ru/'
+
+with webdriver.Chrome(seleniumwire_options=options) as browser:
+    browser.get(url)
+
+    print(browser.find_element(By.ID, 'd_clip_button').find_element(By.TAG_NAME, 'span').text)
+    time.sleep(5)
