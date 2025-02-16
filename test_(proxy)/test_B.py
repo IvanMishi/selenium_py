3560a400376fae4bca3c0d43268ac71dbba8b625
Ivan Mishin <i.a.m@inbox.ru>
2025-02-16 19:21:47 +0300

    upp

--- test_(proxy)/test_B.py
+++ test_(proxy)/test_B.py
@@ -1,16 +1,14 @@
 import time
+from selenium import webdriver
 from selenium.webdriver.common.by import By
-from seleniumwire import webdriver
 
-options = {'proxy': {
-    'http': "socks5://D2Frs6:75JjrW@194.28.210.39:9867",
-    'https': "socks5://D2Frs6:75JjrW@194.28.210.39:9867",
-    }}
-
+proxy = '8.210.83.33:80'
 url = 'https://2ip.ru/'
 
-with webdriver.Chrome(seleniumwire_options=options) as browser:
-    browser.get(url)
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument('--proxy-server=%s' % proxy)
 
+with webdriver.Chrome(options=chrome_options) as browser:
+    browser.get(url)
     print(browser.find_element(By.ID, 'd_clip_button').find_element(By.TAG_NAME, 'span').text)
     time.sleep(5)
