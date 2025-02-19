2e0e2f0d003911903e2b4c7945fba38393ba182f
Ivan Mishin <i.a.m@inbox.ru>
2025-02-19 23:34:27 +0300

    upp

--- test_(proxy)/test_B.py
+++ test_(proxy)/test_B.py
@@ -1,10 +1,13 @@
-import time
-from selenium import webdriver
-from selenium.webdriver.common.by import By
+import time  # Модуль для работы с функцией ожидания
+from selenium import webdriver  # Модуль для взаимодействия с веб-браузерами
 
+proxy = '8.210.83.33:80'
 url = 'https://2ip.ru/'
-with webdriver.Chrome() as webdriver:
-    webdriver.get(url)
-    time.sleep(1)
-    print(webdriver.find_element(By.ID, 'd_clip_button').find_element(By.TAG_NAME, 'span').text)
-    time.sleep(1)
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument('--proxy-server=%s' % proxy)
+
+with webdriver.Chrome(options=chrome_options) as browser:
+    browser.get(url)
+    print(browser.find_element(By.ID, 'd_clip_button').find_element(By.TAG_NAME, 'span').text)
+    time.sleep(5)
