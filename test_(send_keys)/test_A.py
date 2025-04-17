b318f7af4f55b2362cf3bf7f93595d53a614945d
Ivan Mishin <i.a.m@inbox.ru>
2025-04-18 01:15:16 +0300

    upp

--- test_(send_keys)/test_A.py
+++ test_(send_keys)/test_A.py
@@ -7,11 +7,16 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 # импортирует модуль re, который позволяет работать с регулярными выражениями для поиска, замены и обработки текстовых данных по заданным правилам.
 import re
+from faker import Faker # Импортируем класс Faker из установленной библиотеки
+
+
+
 
 # Ссылка на страницу
 link = "http://suninjuly.github.io/simple_form_find_task.html"
 # Измеряет время выполнения определенного участка кода.
 start = time.time()
+fake = Faker("ru_Ru") # Создаём экземпляр класса Faker, указывая, что хотим генерировать данные на (ru_Ru - русский язык)
 
 # Блок try используется для выполнения кода, который может вызвать исключение
 try:
@@ -19,16 +24,18 @@ try:
     webdriver = webdriver.Chrome()
     # Переходит по ссылке
     webdriver.get(link)
-    # Находит и заполняет поле First name
-    input_first_name = webdriver.find_element(By.TAG_NAME, "input").send_keys("Ivan")
-    # Находит и заполняет поле Last name
-    input_last_name = webdriver.find_element(By.NAME, "last_name").send_keys("Petrov")
-    # Находит и заполняет поле City
-    input_сity = webdriver.find_element(By.CLASS_NAME, "form-control.city").send_keys("Smolensk")
-    # Находит и заполняет поле Country
-    input_сountry = webdriver.find_element(By.ID, "country").send_keys("Russia")
+    # Находит и заполняет поле First name фейковыми  данными
+    input_first_name = webdriver.find_element(By.TAG_NAME, "input").send_keys(fake.first_name())
+    # Находит и заполняет поле Last name фейковыми  данными
+    input_last_name = webdriver.find_element(By.NAME, "last_name").send_keys(fake.last_name())
+    # Находит и заполняет поле City фейковыми  данными
+    input_сity = webdriver.find_element(By.CLASS_NAME, "form-control.city").send_keys(fake.city())
+    # Находит и заполняет поле Country фейковыми  данными
+    input_сountry = webdriver.find_element(By.ID, "country").send_keys(fake.country())
     # Находит и нажимает кнопку Submit
     button_submit = webdriver.find_element(By.CSS_SELECTOR, "button.btn").click()
+    # Убеждается, что все данные заполнены успешно.
+    time.sleep(10)
 
     # Получает alert на веб-странице
     alert = webdriver.switch_to.alert
@@ -48,5 +55,3 @@ finally:
 
     # Закрывает браузер
     webdriver.quit()
-
-# Не забывает оставить пустую строку в конце файла
