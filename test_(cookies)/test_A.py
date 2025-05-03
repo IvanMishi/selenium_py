e64b290bb78dc78bcce72f591ca82a071edad4f9
IvanMishi <i.a.m@inbox.ru>
2025-05-03 23:52:06 +0300

    origin

--- test_(cookies)/test_A.py
+++ test_(cookies)/test_A.py
@@ -1,32 +1,25 @@
 import time  # Модуль для работы с функцией ожидания
 from selenium import webdriver  # Модуль для взаимодействия с веб-браузерами
-from pprint import pprint  # Модуль для "понятной печати" структур данных, таких как словари и списки.
-from selenium.webdriver.common.by import By
+from selenium.webdriver.common.by import By  # Модуль для определения способов поиска элементов на странице
+from selenium.webdriver.support.ui import WebDriverWait  # Модуль для реализации явно-ожидаемых условий
+from selenium.webdriver.support import expected_conditions as EC  # Модуль для работы с ожидаемыми условиями
 
 # Ссылка на страницу.
-link = 'https://parsinger.ru/selenium/6/6.3.2/index.html '
+link = 'https://parsinger.ru/selenium/6/6.3.2/index.html'
 # Измеряет время выполнения.
 start = time.time()
 
-with webdriver.Chrome() as webdriver:  # Создаёт экземпляр драйвера Chrome и автоматически закрывает его по завершении блока кода.
-    webdriver.get(link)  # Переходит по ссылке.
+with webdriver.Chrome() as driver:  # Создаёт экземпляр драйвера Chrome и автоматически закрывает его по завершении блока кода.
+    driver.get(link)  # Переходит по ссылке.
+
     time.sleep(1)  # Убеждается что открыта искомая страница.
+    assert link == driver.current_url, f'\nОжидаемый   URL: {link}, \nФактический URL: {driver.current_url}'  # Ошибка будет выведена в консоль в случае если URL не совпадают
 
+    # Удаляет все cookies текущей сессии браузера
+    driver.delete_all_cookies()
+    # Ожидает появление элемента для вывода текста из него в качестве ответа. 
+    print(f'Ответ: {WebDriverWait(driver, 60).until(EC.visibility_of_element_located((By.ID, "password"))).text}')
 
-    # Получает все куки из текущего браузера с помощью метода get_cookies() объекта webdriver
-    cookies = webdriver.get_cookies()
-
-    # Создаёт список для хранения значений куки.
-    find_cookie_values = []
-
-    # Начинаем цикл, чтобы пройтись по каждому элементу в списке cookies
-    for cookie in cookies:
-        print(cookie['name'])
-        
-        input_area = webdriver.find_element(By.ID,'phraseInput').send_keys(str(cookie['name']))
-        check_button = webdriver.find_element(By.ID, 'checkButton').click()
-        print(f'Ответ: {webdriver.find_element(By.ID,'result').text }')
-    time.sleep(1)
 
 # Завершение отсчета времени
 end = time.time()
