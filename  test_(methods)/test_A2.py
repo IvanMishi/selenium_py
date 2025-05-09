31162d3961a7e74f250e4231cb798650fdb6db72
IvanMishi <i.a.m@inbox.ru>
2025-05-09 22:40:15 +0300

    origin

---  test_(methods)/test_A2.py
+++  test_(methods)/test_A2.py
@@ -2,7 +2,7 @@ import time  # Модуль для работы с функцией ожидан
 from selenium import webdriver  # Модуль для взаимодействия с веб-браузерами
 
 # Cсылка на страницу
-link = "https://parsinger.ru/selenium/3/3.2.3/index.html"
+link = "https://parsinger.ru/selenium/3/3.2.2/index.html"
 # Измеряет время выполнения определенного участка кода.
 start = time.time()
 
@@ -13,16 +13,12 @@ with webdriver.Chrome() as driver:  # Создаёт экземпляр драй
     # Ошибка будет выведена в консоль в случае если URL не совпадают.
     assert link == driver.current_url, f'\nОжидаемый   URL: {link}, \nФактический URL: {driver.current_url}'
 
-    print('Находит кнопку на веб-странице и нажимает на нее.')
-    show_button = driver.find_element("id", "showTextBtn").click()
-    print('Находит элемент с данными м сохраняет текст в переменную')
-    text_element = driver.find_element('id', 'text1').text
-    print('Находит поле для ввода текста на веб-странице и заполняет данными.')
-    textarea = driver.find_element("id", "userInput").send_keys(text_element)
-    print('Находит кнопку проверки кода на веб-странице и нажимает на нее')
-    check_button = driver.find_element("id", "checkBtn").click()
+    print('Находит поле для ввода текста на веб-странице и заполняет его пользовательским текстом.')
+    textarea = driver.find_element("id", "codeInput").send_keys('Дрогон')
+    print('Находит кнопку на веб-странице и выполняет по ней клик.')
+    pass_button = driver.find_element("id", "clickButton").click()
     print('Выводит текст из появившегося элемента  в качестве ответа в консоль.')
-    print(f'Ответ: {driver.find_element("id", "text2").text}')
+    print(f'Ответ: {driver.find_element("id", "codeOutput").text}')
 
 
 # Завершение отсчета времени
---  test_(methods)/test_A3.py
+++  test_(methods)/test_A3.py
@@ -2,19 +2,27 @@ import time  # Модуль для работы с функцией ожидан
 from selenium import webdriver  # Модуль для взаимодействия с веб-браузерами
 
 # Cсылка на страницу
-link = "https://parsinger.ru/selenium/3/3.2.4/index.html"
+link = "https://parsinger.ru/selenium/3/3.2.3/index.html"
 # Измеряет время выполнения определенного участка кода.
 start = time.time()
 
 
-with webdriver.Chrome() as webdriver:  # Создаёт экземпляр драйвера Chrome и автоматически закрывает его по завершении блока кода.
-    webdriver.get(link)  # Переходит по ссылке.
+with webdriver.Chrome() as driver:  # Создаёт экземпляр драйвера Chrome и автоматически закрывает его по завершении блока кода.
+    driver.get(link)  # Переходит по ссылке.
     time.sleep(1)  # Убеждается что открыта искомая страница.
+    # Ошибка будет выведена в консоль в случае если URL не совпадают.
+    assert link == driver.current_url, f'\nОжидаемый   URL: {link}, \nФактический URL: {driver.current_url}'
 
-    # Находит кнопку на веб-странице и нажимает на нее
-    find_button = webdriver.find_element('id', 'secret-key-button').click()
-    # Вывводит значение атрибута в качестве ответа в консоль
-    print(f'Ответ: {webdriver.find_element('id','secret-key-button').get_attribute('data')}')
+    print('Находит кнопку на веб-странице и нажимает на нее.')
+    show_button = driver.find_element("id", "showTextBtn").click()
+    print('Находит элемент с данными м сохраняет текст в переменную')
+    text_element = driver.find_element('id', 'text1').text
+    print('Находит поле для ввода текста на веб-странице и заполняет данными.')
+    textarea = driver.find_element("id", "userInput").send_keys(text_element)
+    print('Находит кнопку проверки кода на веб-странице и нажимает на нее')
+    check_button = driver.find_element("id", "checkBtn").click()
+    print('Выводит текст из появившегося элемента  в качестве ответа в консоль.')
+    print(f'Ответ: {driver.find_element("id", "text2").text}')
 
 
 # Завершение отсчета времени
