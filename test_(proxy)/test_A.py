150d102555ca6977ff1f9328116f727bab04d7b4
IvanMishi <i.a.m@inbox.ru>
2025-05-12 22:44:59 +0300

    origin

--- test_(proxy)/test_A.py
+++ test_(proxy)/test_A.py
@@ -1,19 +1,31 @@
 import time  # Модуль для работы с функцией ожидания
 from selenium import webdriver  # Модуль для взаимодействия с веб-браузерами
 from selenium.webdriver.common.by import By  # Модуль для определения способов поиска элементов на странице
+from selenium.webdriver.common.alert import Alert
+
 
 # Создает объект ChromeOptions для настройки параметров браузера Chrome.
 options_chrome = webdriver.ChromeOptions()
 # Добавляет расширение (в данном случае 'coordinates.crx') в браузер Chrome.
 #options_chrome.add_extension('coordinates.crx')
 
-# Cсылка на страницу
+# Ссылка на страницу
 link = 'https://2ip.ru/'
-with webdriver.Chrome(options=options_chrome) as webdriver:  # Создаёт экземпляр драйвера Chrome и автоматически закрывает его по завершении блока кода.
-    webdriver.get(link) # Переходит по ссылке.
-    # Ждет 1 секунду
+# Измеряет время выполнения
+start = time.time()
+
+
+with webdriver.Chrome() as driver:
+    # Переходит по ссылке
+    driver.get(link)
+    time.sleep(1)  # Убеждается что открыта искомая страница
+    # Ошибка будет выведена в консоль в случае если URL не совпадают.
+    assert link == driver.current_url, f'\nОжидаемый   URL: {link}, \nФактический URL: {driver.current_url}'
+
+    print('Находит элемент по ID "d_clip_button", затем находим вложенный элемент span, и выводит его текст в консоль.')
+    print(f'Ваш IP: {driver.find_element(By.ID, "d_clip_button").find_element(By.TAG_NAME, "span").text}')
     time.sleep(1)
-    # Находит элемент по ID 'd_clip_button', затем находим вложенный элемент span, и выводим его текст в консоль.
-    print(webdriver.find_element(By.ID, 'd_clip_button').find_element(By.TAG_NAME, 'span').text)
-    # Ждет 1 секунду
-    time.sleep(1)
+
+# Завершение отсчета времени
+end = time.time()
+print(f"Время выполнения: {end - start} секунд.")
