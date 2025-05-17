5197e97c03f43817d346dedb701fbb2422a2f392
IvanMishi <i.a.m@inbox.ru>
2025-05-18 01:17:40 +0300

    origin

---  test_(methods)/test_H.py
+++  test_(methods)/test_H.py
@@ -1,6 +1,7 @@
 import time  # Модуль для работы с функцией ожидания
 from selenium import webdriver  # Модуль для взаимодействия с веб-браузерами
 from selenium.webdriver import Keys
+from selenium.webdriver.common import keys
 from selenium.webdriver.common.by import By  # Модуль для определения способов поиска элементов на странице
 from faker import Faker # Импортируем класс Faker из установленной библиотеки
 import datetime # Модуль  для работы с датами и временем
@@ -22,25 +23,34 @@ with webdriver.Chrome() as driver:
     # Ошибка будет выведена в консоль в случае если URL не совпадают.
     assert link == driver.current_url, f'\nОжидаемый   URL: {link}, \nФактический URL: {driver.current_url}'
 
+    # Создание локатора ндля выбора даты на 10 дней вперед от текущей
+    print('Определяет текущую дату')
+    now_date = datetime.datetime.utcnow().strftime("%m,%d,%Y")
+    print(f'Сегодняшняя дата {now_date.replace(",", "/")}')
+    print('Устанавливает к текущей дате 10 дней')
+    date_future = int(now_date.split(",")[1]) + 10
+    print(f'Дата через десять дней: {date_future} число')
+    print('Создает локатор для выбора элемента на 10 днй вперед от текущей даты')
+    locator_new_date = f"//div[@aria-label='Choose Tuesday, May {date_future}th, 2025']"
+    print(f'Синтаксис созданого локатора: {locator_new_date}')
+
+
     print('Находит поле для выбора даты')
     date_area = driver.find_element(By.XPATH, "//input[@id='datePickerMonthYearInput']")
     print('Удаляет текущую дату')
     [date_area.send_keys(Keys.BACKSPACE) for _ in range(10)]
+    print('Визуально убеждается что текущая дата удалена')
+    time.sleep(3)
+    print('Устаеавливает текущую дату повторно')
+    date_area.send_keys(f'{now_date.replace("," , "/")}')
+    print('Визуально убеждается что текущая дата установлена')
+    time.sleep(3)
 
-    print('Определяет текущую дату')
-    now_date = datetime.datetime.utcnow().strftime("%d")
-    print(f'Сегодняшняя дата {now_date}')
-    print('Устанавливает к текущей дате 10 дней')
-    date_future = int(now_date) + 10
-    print('Создает локатор для выбора элемента на 10 днй вперед от текущей даты')
-    locator_new_date = f"//div[@aria-label='Choose Tuesday, May {date_future}th, 2025']"
-    print(f'Синтаксис созданого локатора: {locator_new_date}')
     print('Находит дату по локатору')
-    time.sleep(5)
     date_last_10 = driver.find_element(By.XPATH, locator_new_date)
-    time.sleep(5)
     print('Выбирает найденную дату')
     date_last_10.click()
-    print('Визуально убеждается что все действия выполнены успешно')
-    time.sleep(10)
+    date_area.send_keys(Keys.RETURN)
+    print('Визуально убеждается дата установлена успешно')
+    time.sleep(5)
 
